9b17d6d7ead1b4b23917248f9a8f2ba61d0f6206
MuraliprakashK <102814350+MuraliprakashK@users.noreply.github.com>
2022-10-31 20:20:53 +0530

    Update System volume.py

--- System volume.py
+++ System volume.py
@@ -5,6 +5,7 @@ from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 import numpy as np
+#murali
 
 
 cap = cv2.VideoCapture(0)
